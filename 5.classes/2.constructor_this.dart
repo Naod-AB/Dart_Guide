4165069e72343e11e5ac6608bf8784d2e574db2b
Naod <naodat.dev@gmail.com>
2023-05-10 09:32:02 +0300

    constructor and this

---
+++ 5.classes/2.constructor_this.dart
@@ -0,0 +1,31 @@
+class BankAccount {
+  // constructors
+  BankAccount(double balance) {
+    this.balance = balance;
+  }
+  double balance = 0;
+
+  void deposit(double amount) => balance += amount;
+  bool withdraw(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
+}
+
+void main() {
+  final account1 = BankAccount(100); //instances of a class
+  final account2 = BankAccount(200);
+  // account1.balance = 100;
+  // account2.balance = 50;
+
+  account1.deposit(50);
+  account2.deposit(500);
+  final withdraw1 = account1.withdraw(30);
+  final withdraw2 = account2.withdraw(5000);
+
+  print('Success: $withdraw1, balance: ${account1.balance}');
+  print('Success: $withdraw2, balance: ${account2.balance}');
+}
