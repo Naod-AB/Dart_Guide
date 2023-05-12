f2536ebd6c4fcac62a99c28001afc3a35ef0441c
Naod <naodat.dev@gmail.com>
2023-05-12 14:32:53 +0300

    simple ecommerce program

---
+++ 7.ecommerce/lib/all_products.dart
@@ -0,0 +1,25 @@
+import 'package:test/product.dart';
+
+const allProducts = [
+  Product(id: 1, name: 'apples', price: 1.60),
+  Product(id: 2, name: 'bananas', price: 0.70),
+  Product(id: 3, name: 'courgettes', price: 1.0),
+  Product(id: 4, name: 'grapes', price: 2.00),
+  Product(id: 5, name: 'mushrooms', price: 0.80),
+  Product(id: 6, name: 'potatoes', price: 1.50),
+];
+
+
+
+// loop
+//   prompt: view cart / add item / checkout
+//   if selection == add item
+//     choose a product
+//     add it to the cart
+//     print cart
+//   else if selection == view cart
+//     print cart
+//   else if selection == checkout
+//     do checkout
+//     exit
+//   end
---
+++ 7.ecommerce/lib/check_out.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'cart.dart';
+
+bool checkout(Cart cart) {
+  if (cart.isEmpty) {
+    print('Cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total: \$$total');
+  stdout.write('Payment in cash: ');
+  final line = stdin.readLineSync();
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line);
+  if (paid == null) {
+    return false;
+  }
+  if (paid >= total) {
+    final change = paid - total;
+    print('Change: \$${change.toStringAsFixed(2)}');
+    return true;
+  } else {
+    print('Not enough cash.');
+    return false;
+  }
+}
