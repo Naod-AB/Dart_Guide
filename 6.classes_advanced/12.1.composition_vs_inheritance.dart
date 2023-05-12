eeddb8ce5f83991bbac24287535019ede2b19675
Naod <naodat.dev@gmail.com>
2023-05-12 12:09:08 +0300

    composition vs inheritcance with example &pic

---
+++ 6.classes_advanced/12.1.composition_vs_inheritance.dart
@@ -0,0 +1,24 @@
+// Generic class that push and pops different types
+
+class Stack<T> {
+  final List<T> _items = [];
+
+  void push(T item) => _items.add(item);
+
+  T pop() => _items.removeLast();
+}
+
+void main() {
+  final stack = Stack<int>();
+
+  stack.push(1);
+  stack.push(2);
+  print(stack.pop());
+  print(stack.pop());
+
+  final names = Stack<String>();
+  names.push('Hello');
+  names.push('World');
+  print(names.pop());
+  print(names.pop());
+}
